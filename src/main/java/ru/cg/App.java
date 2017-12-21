9bca89e11923b27c8bef7fc18090fc921bd404a9
Rinat Suleymanov <rinat@suleymanov.su>
2017-12-21 12:45:23 +0300

    change start method
    (cherry picked from commit e00ec22)

--- src/main/java/ru/cg/App.java
+++ src/main/java/ru/cg/App.java
@@ -1,5 +1,10 @@
 package ru.cg;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.zaxxer.hikari.HikariDataSource;
+
 import ru.cg.datasource.DataSourceProvider;
 import ru.cg.model.Project;
 
@@ -10,10 +15,15 @@ public class App {
 
   public static void main(String[] args) {
     DataSourceProvider provider = new DataSourceProvider();
-    FiasConverter fiasConverter = new FiasConverter(
-        Project.ZAGS,
-        provider.createProjectDataSource(),
-        provider.createFiasDataSource());
-    fiasConverter.convert();
+    HikariDataSource projectDataSource = provider.createProjectDataSource();
+    HikariDataSource fiasDataSource = provider.createFiasDataSource();
+
+    List<FiasConverter> converters = new ArrayList<>();
+    converters.add(new FiasConverter(Project.GAI_APPLICANT, projectDataSource, fiasDataSource));
+    converters.add(new FiasConverter(Project.GAI_DIVISION_ADDRESS, projectDataSource, fiasDataSource));
+    converters.add(new FiasConverter(Project.GAI_OBJECT_ADDRESS, projectDataSource, fiasDataSource));
+    converters.add(new FiasConverter(Project.GAI_ORGANIZATION_VERSION_ADDRESS, projectDataSource, fiasDataSource));
+
+    converters.forEach(FiasConverter::convert);
   }
 }
